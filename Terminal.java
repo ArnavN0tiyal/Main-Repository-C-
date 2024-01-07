548a14dd9d81d542ef7d0f69b58c7d282e0a52bf
YourAverageCoder <125085107+ArnavN0tiyal@users.noreply.github.com>
2024-01-07 15:21:47 +0530

    Update Terminal.java
    Added two new features - Program opening and shutdown
    Made the code more easy to read

--- Terminal.java
+++ Terminal.java
@@ -7,11 +7,10 @@ public class Terminal {
     static String username = "User";
     static String brand = "Dell";
     static Process proc;
-  public static void main(String[] args) throws IOException {
-    LocalTime time = LocalTime.now();
-    Scanner sc = new Scanner(System.in);
-    Runtime run = Runtime.getRuntime();
-    System.out.println("Nevertx NeVerox [Version 1.0]\n(c) Nevertx. All rights unreserved.\n");
+    static Scanner sc = new Scanner(System.in);
+    static Runtime run = Runtime.getRuntime();
+public static void main(String[] args) throws IOException {
+    System.out.println("Nevertx NeVerox [Version 1.1]\n(c) Nevertx. All rights unreserved.\n");
     do {
         if (print == 1) {
             System.out.print("C:\\" + username + "\\" + brand + ">");
@@ -19,97 +18,325 @@ public class Terminal {
             System.out.print("");
         }
         String cmd = sc.nextLine();
-        if (cmd.equalsIgnoreCase("help")) {
-            System.out.println("ECHO         Prints a string on the console or turns on or off command echoing\nVERIFY       Verifies if that the files are correctly written to the Devops disk\nTIME         Shows the time\nEXIT         Exits the terminal\nADD          Adds two numbers\nSUB          Subtracts two numbers\nMUL          Multiplies two numbers\nDIV          Divides two numbers\nUSERNAME     Sets your username\nBRANDTYPE    Sets the user's computer brand\nOPEN         Opens a program. To see the list of programs that work, type \"programs\" in the command line\n");
-        } else if (cmd.equalsIgnoreCase("echo")) {
-            System.out.print("Type the sentence you want to print: ");
-            String line = sc.nextLine();
-            System.out.println(line + "\n");
-        } else if (cmd.equalsIgnoreCase("verify")) {
-            int chance = (int)(1 + Math.random()* 1000);
-            if (chance >= 1) {
-                System.out.println("Files are correctly written to the disk.\n");
-            } else if (chance == 1000) {
-                System.out.println("Files aren't written correctly to the disk. Please restart the terminal.\n");
-                sc.close();
+        executeCmd(cmd);
+    } while (exit != 0);
+  }
+public static void executeCmd(String cmd) throws IOException {
+            switch(cmd) {
+                case "help":
+                case "Help":
+                case "HELP":
+                help();
+                break;
+                case "echo":
+                case "Echo":
+                case "ECHO":
+                echo();
+                break;
+                case "echo on":
+                case "Echo on":
+                case "Echo On":
+                case "ECHO ON":
+                echo_on();
+                break;
+                case "echo off":
+                case "Echo off":
+                case "Echo Off":
+                case "ECHO OFF":
+                echo_off();
+                break;
+                case "verify":
+                case "Verify":
+                case "VERIFY":
+                verify();
+                break;
+                case "time":
+                case "Time":
+                case "TIME":
+                showtime();
+                break;
+                case "exit":
+                case "Exit":
+                case "EXIT":
+                exit();
+                break;
+                case "add":
+                case "Add":
+                case "ADD":
+                addition();
+                break;
+                case "sub":
+                case "Sub":
+                case "SUB":
+                subtraction();
+                break;
+                case "mul":
+                case "Mul":
+                case "MUL":
+                multiplication();
+                break;
+                case "div":
+                case "Div":
+                case "DIV":
+                division();
+                break;
+                case "username":
+                case "Username":
+                case "USERNAME":
+                username();
+                break;
+                case "brandtype":
+                case "Brandtype":
+                case "BrandType":
+                case "BRANDTYPE":
+                brandtype();
+                break;
+                case "open":
+                case "Open":
+                case "OPEN":
+                applications();
+                break;
+                case "programs":
+                case "Programs":
+                case "PROGRAMS":
+                programs();
+                break;
+                case "shutdown /?":
+                case "Shutdown /?":
+                case "SHUTDOWN /?":
+                case "shutdown":
+                case "Shutdown":
+                case "SHUTDOWN":
+                shutdown();
+                break;
+                case "shutdown /i":
+                case "Shutdown /i":
+                case "SHUTDOWN /i":
+                shutdown_1();
+                break;
+                case "shutdown /l":
+                case "Shutdown /l":
+                case "SHUTDOWN /l":
+                shutdown_2();
+                break;
+                case "shutdown /s":
+                case "Shutdown /s":
+                case "SHUTDOWN /s":
+                shutdown_3();
+                break;
+                case "shutdown /sg":
+                case "Shutdown /sg":
+                case "SHUTDOWN /sg":
+                shutdown_4();
+                break;
+                case "shutdown /r":
+                case "Shutdown /r":
+                case "SHUTDOWN /r":
+                shutdown_5();
+                break;
+                case "shutdown /g":
+                case "Shutdown /g":
+                case "SHUTDOWN /g":
+                shutdown_6();
+                break;
+                case "shutdown /a":
+                case "Shutdown /a":
+                case "SHUTDOWN /a":
+                shutdown_7();
+                break;
+                case "shutdown /p":
+                case "Shutdown /p":
+                case "SHUTDOWN /p":
+                shutdown_8();
+                break;
+                case "shutdown /h":
+                case "Shutdown /h":
+                case "SHUTDOWN /h":
+                shutdown_9();
+                break;
+                default:
+                System.out.println("\'" + cmd + "\'" + " is not recognized as a internal or external command.\n");
+                break;
             }
-        } else if (cmd.equalsIgnoreCase("time")) {
-            System.out.println("The current time is: " + time + "\n");
-        } else if (cmd.equalsIgnoreCase("exit")) {
-            exit = 0;
+}
+
+public static void help() {
+    System.out.println("ECHO         Prints a string on the console or turns on or off command echoing\nVERIFY       Verifies if that the files are correctly written to the Devops disk\nTIME         Shows the time\nEXIT         Exits the terminal\nADD          Adds two numbers\nSUB          Subtracts two numbers\nMUL          Multiplies two numbers\nDIV          Divides two numbers\nUSERNAME     Sets your username\nBRANDTYPE    Sets the user's computer brand\nOPEN         Opens a program. To see the list of programs that work, type \"programs\" in the command line\nSHUTDOWN     Allows proper and local shutdown of the computer\n");
+}
+
+public static void echo() {
+    System.out.print("Type the sentence you want to print: ");
+    String line = sc.nextLine();
+    System.out.println(line + "\n");
+}
+
+public static void echo_on() {
+    print = 0;
+}
+
+public static void echo_off() {
+    print = 1;
+}
+
+public static void verify() {
+        int chance = (int)(1 + Math.random()* 1000);
+        if (chance >= 1) {
+            System.out.println("Files are correctly written to the disk.\n");
+        } else if (chance == 1000) {
+            System.out.println("Files aren't written correctly to the disk. Please restart the terminal.\n");
             sc.close();
-        } else if (cmd.equalsIgnoreCase("add")) {
-            System.out.print("Type the first addend: ");
-            double addend1 = sc.nextDouble();
-            System.out.print("Type the second addend: ");
-            double addend2 = sc.nextDouble();
-            double sum = addend1 + addend2;
-            System.out.println("The sum of " + addend1 + " and " + addend2 + " is " + sum + ".\n");
-        } else if (cmd.equalsIgnoreCase("sub")) {
-            System.out.print("Type the mineund: ");
-            double mineund = sc.nextDouble();
-            System.out.print("Type the subtrahend: ");
-            double subtrahend = sc.nextDouble();
-            double difference = mineund - subtrahend;
-            System.out.println("The difference of " + mineund + " and " + subtrahend + " is " + difference + ".\n");
-        } else if (cmd.equalsIgnoreCase("mul")) {
-            System.out.print("Type the multiplier: ");
-            double multiplier = sc.nextDouble();
-            System.out.print("Type the multiplicand: ");
-            double multiplicand = sc.nextDouble();
-            double product = multiplier*multiplicand;
-            System.out.println("The product of " + multiplier + " and " + multiplicand + " is " + product + ".\n");
-        } else if (cmd.equalsIgnoreCase("div")) {
-            System.out.print("Type the dividend: ");
-            double dividend = sc.nextDouble();
-            System.out.print("Type the divisor: ");
-            double divisor = sc.nextDouble();
-            if (divisor != 0) {
-            double quotient = dividend/divisor;
-            double remainder = dividend % divisor;
-            System.out.println("The quotient of " + dividend + " and " + divisor + " is " + quotient + " and the remainder is " + remainder + ".\n");
-        } else {
-            System.out.println("Can't divide by zero.\n");
         }
-        } else if (cmd.equalsIgnoreCase("echo on")) {
-            print = 1;
-        } else if (cmd.equalsIgnoreCase("echo off")) {
-            print = 0;
-        } else if (cmd.equalsIgnoreCase("username")) {
-            System.out.print("Type the username you want it to be: ");
-            username = sc.nextLine();
-            System.out.println("Successfully set the username to: " + username + ".\n");
-        } else if (cmd.equalsIgnoreCase("brand")) {
-            System.out.print("Type your laptop brand: ");
-            brand = sc.nextLine();
-            System.out.println("Successfully changed the laptop brand to: " + brand + ".\n");
-        } else if (cmd.equalsIgnoreCase("open")) {
-            System.out.print("Type the program you want to open: ");
-            String program = sc.nextLine();
-            try {
-            if (program.equalsIgnoreCase("chrome")) {
-                proc = run.exec(new String[]{"chrome","/C","Start"});
-            } else if (program.equalsIgnoreCase("edge")) {
-                proc = run.exec(new String[]{"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"});
-            } else if (program.equalsIgnoreCase("Command Prompt") ||
-            program.equalsIgnoreCase("cmd")){
-                proc = run.exec(new String[]{"cmd","/C","Start"});
-            } else if (program.equalsIgnoreCase("calc") ||
-            program.equalsIgnoreCase("calculator")) {
-                proc = run.exec(new String[]{"calc","/C","Start"});
-            } else {
-                System.out.println("Unknown Program. Type a vaild program name.\n");
-            }
-            System.out.println("Running: " + program + "\n"); 
-            }
-            catch(Exception ex) {
-                System.out.println("Failed to execute.\n");
-            }
-        } else if (cmd.equalsIgnoreCase("programs")) {
-            System.out.println("1) CHROME\n2) EDGE\n3) EXPLORER\n4) VSCODE\n5) CALCULATOR\n");
-        } else {
-            System.out.println("\'" + cmd + "\'" + " is not recognized as a internal or external command.");
-        }
-  } while (exit != 0);
+}
+
+public static void showtime() {
+    LocalTime time = LocalTime.now();
+    System.out.println("The current time is: " + time + "\n");
+}
+
+public static void exit() {
+    exit = 0;
+    sc.close();
+}
+
+public static void addition() {
+    System.out.print("Type the first addend: ");
+    double addend1 = sc.nextDouble();
+    System.out.print("Type the second addend: ");
+    double addend2 = sc.nextDouble();
+    double sum = addend1 + addend2;
+    System.out.println("The sum of " + addend1 + " and " + addend2 + " is " + sum + ".\n");
+}
+
+public static void subtraction() {
+    System.out.print("Type the mineund: ");
+    double mineund = sc.nextDouble();
+    System.out.print("Type the subtrahend: ");
+    double subtrahend = sc.nextDouble();
+    double difference = mineund - subtrahend;
+    System.out.println("The difference of " + mineund + " and " + subtrahend + " is " + difference + ".\n");
+}
+
+public static void multiplication() {
+    System.out.print("Type the multiplier: ");
+    double multiplier = sc.nextDouble();
+    System.out.print("Type the multiplicand: ");
+    double multiplicand = sc.nextDouble();
+    double product = multiplier*multiplicand;
+    System.out.println("The product of " + multiplier + " and " + multiplicand + " is " + product + ".\n");
+}
+
+public static void division() {
+    System.out.print("Type the dividend: ");
+    double dividend = sc.nextDouble();
+    System.out.print("Type the divisor: ");
+    double divisor = sc.nextDouble();
+    if (divisor != 0) {
+    double quotient = dividend/divisor;
+    double remainder = dividend % divisor;
+    System.out.println("The quotient of " + dividend + " and " + divisor + " is " + quotient + " and the remainder is " + remainder + ".\n");
+} else {
+    System.out.println("Can't divide by zero.\n");
+ }
+}
+
+public static void username() {
+    System.out.print("Type the username you want it to be: ");
+    username = sc.nextLine();
+    System.out.println("Successfully set the username to: " + username + ".\n");
+}
+
+public static void brandtype() {
+    System.out.print("Type your laptop brand: ");
+    brand = sc.nextLine();
+    System.out.println("Successfully changed the laptop brand to: " + brand + ".\n");
+}
+
+public static void applications() {
+    System.out.print("Type the program you want to open: ");
+    String program = sc.nextLine();
+    try {
+    switch(program) {
+        case "edge":
+        case "Edge":
+        case "EDGE":
+        proc = run.exec(new String[]{"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"});
+        break;
+        case "command prompt":
+        case "Command Prompt":
+        case "COMMAND PROMPT":
+        case "cmd":
+        case "Cmd":
+        case "CMD":
+        proc = run.exec(new String[]{"cmd","/C","Start","Nevertx Promopt"});
+        break;
+        case "calculator":
+        case "CALCULATOR":
+        case "calc":
+        case "CALC":
+        proc = run.exec(new String[]{"calc","/C","Start"});
+        break;
+        case "vscode":
+        case "Vscode":
+        case "VSCODE":
+        case "visual studio code":
+        case "Visual studio code":
+        case "Visual Studio Code":
+        case "VISUAL STUDIO CODE":
+        case "code":
+        case "Code":
+        case "CODE":
+        proc = run.exec(new String[]{"cmd","/K","code"});
+        break;
+        default:
+        System.out.println("Unknown Program. Type a vaild program name.\n");
+        break;
+     }
+    }
+    catch(Exception ex) {
+        System.out.println("Failed to execute.\n");
+    }
+}
+
+public static void programs() {
+    System.out.println("1) EDGE\n2) EXPLORER\n3) VSCODE\n4) CALCULATOR\n");
+}
+
+public static void shutdown() {
+    System.out.println("How to use: shutdown /i, /l, /s, /sg, /r, /g, /a, /p, /h\nNo args    This is the same as doing /?\n/i         Shows the graphical user interface\n/?         Gives the type of shutdown the user can do\n/l         Logs off the computer\n/s         Complete shutdown of the computer\n/sg        Shutdowns the computer. Then on the next boot, if automatic sign in is enabled then it will automaticly sign in to the user's computer\n/r         Shutdowns then restarts\n/g         Shutdowns and restarts. After the system is rebooted, if automatic sign in is enabled, the user will automaticly sign in\n/a         Abort a system shutdown *Can only be used in time out period*\n/p         Turns off the computer without any warning or time out\n/h         Hibernate the local computer\n");
+}
+public static void shutdown_1() throws IOException {
+    proc = run.exec(new String[]{"cmd","/C","Start","shutdown", "/i"});
+}
+
+public static void shutdown_2() throws IOException {
+    proc = run.exec(new String[]{"cmd","/C","Start","shutdown", "/l"});
+}
+
+public static void shutdown_3() throws IOException {
+    proc = run.exec(new String[]{"cmd","/C","Start","shutdown", "/s"});
+}
+
+public static void shutdown_4() throws IOException {
+    proc = run.exec(new String[]{"cmd","/C","Start","shutdown", "/sg"});
+}
+
+public static void shutdown_5() throws IOException {
+    proc = run.exec(new String[]{"cmd","/C","Start","shutdown", "/r"});
+}
+
+public static void shutdown_6() throws IOException {
+    proc = run.exec(new String[]{"cmd","/C","Start","shutdown", "/g"});
+}
+
+public static void shutdown_7() throws IOException {
+    proc = run.exec(new String[]{"cmd","/C","Start","shutdown", "/a"});
+}
+
+public static void shutdown_8() throws IOException {
+    proc = run.exec(new String[]{"cmd","/C","Start","shutdown", "/p"});
+}
+
+public static void shutdown_9() throws IOException {
+    proc = run.exec(new String[]{"cmd","/C","Start","shutdown", "/h"});
  }
 }
